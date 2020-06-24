a168e7bdd2148d9ed58d7ff157988866fd0a6e85
Andrej Krnac <6131252+adys@users.noreply.github.com>
2020-06-24 17:57:01 +0200

    Add curl options var

--- init.sh
+++ init.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
 GIT_RELEASE=${1:-0.1.0}
+CURL_OPTIONS=$2
 
-URL_SCRIPTS="https://raw.githubusercontent.com/lablabs/aws-sf-userdata/${GIT_RELEASE}/"
-
+URL_SCRIPTS="https://raw.githubusercontent.com/lablabs/aws-sf-userdata/${GIT_RELEASE}"
 OS="$(lsb_release -c -s)"
 OS_USER_DATA="${URL_SCRIPTS}/${OS}.sh"
 
-if wget --spider $OS_USER_DATA 2>/dev/null; then
-  curl -s -L $OS_USER_DATA | bash
-else
+http_code="$(curl $CURL_OPTIONS --write-out "%{http_code}\n" --silent --output /dev/null $OS_USER_DATA)"
+
+if [[ "$http_code" -eq 200 ]]; then
+  curl $CURL_OPTIONS -s -L $OS_USER_DATA > out
+elif [[ "$http_code" -eq 404 ]]; then
   echo "${OS} is not supported"
+  exit 1
+else
+  echo "curl failed"
   exit 1
 fi
